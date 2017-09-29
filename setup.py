af7b8c0d20e7bc9fc1e1b22f03cd396ce02f6fad
cvsper <cvsper813@gmail.com>
2017-09-29 13:38:42 -0400

    update docs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='moviePy',
+	name='cinemaPy',
 	version='0.1.0',
-	description='Watch movies',
+	description='Watch streaming movies',
 	license='MIT',
-	install_requires=['requests','BeautifulSoup' ],
+	install_requires=['requests','BeautifulSoup', 'lxml', 'html.parser' ],
 	packages=find_packages(),
 	scripts='moviepy.py'
 
